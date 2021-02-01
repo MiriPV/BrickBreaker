d7681434ae97bbe93834e324b3701c8e9e0a8a72
MiriPV <mirandaverheij93@gmail.com>
2021-02-01 21:31:40 +0100

    Added first part of gameplay

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,2 +1,60 @@
-public class GamePlay {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+
+    private int totalBricks = 21;
+
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballPositionX = 120;
+    private int ballPositionY = 350;
+    private int ballDirectionX = -1;
+    private int ballDirectionY = -2;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics graphics) {
+        //Background
+        graphics.setColor(Color.black);
+        graphics.fillRect(1, 1, 692, 592);
+
+        //Borders
+        graphics.setColor(Color.yellow);
+        graphics.fillRect(0, 0, 3, 592);
+        graphics.fillRect(0, 0, 692, 3);
+        graphics.fillRect(691, 0, 3, 592);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+
         window.setBounds(10, 10, 700, 600);
         window.setTitle("Brick Breaker");
         window.setResizable(true);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.add(gamePlay);
 
     }
 }
