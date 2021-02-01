746b42e350c6a48e752daf36e077e9d923cc3a36
MiriPV <mirandaverheij93@gmail.com>
2021-02-01 21:03:13 +0100

    Created window

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setBounds(10, 10, 700, 600);
+        window.setTitle("Brick Breaker");
+        window.setResizable(true);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
